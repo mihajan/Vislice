0c87b740e9685bac83e1cafc49ab6ac491199ebe
Miha Jan <miha.jan2000@gmail.com>
2020-05-04 18:39:06 +0200

    dodan program za praštevila in imena matematikov

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Vislice
 Repozitorij projekta vislice, ki ga bomo skupaj delali na vajah pri predmetu UVP.
+
+Matematiki
+
+Josip Plemlelj
+Jurij Vega
+Ivan Vidav
---
+++ test2.py
@@ -0,0 +1,16 @@
+#napišimo program, ki izpiše praštevila do 200
+
+print(2)
+print(3)
+
+for j in range(4, 200):
+
+    je_prastevilo = True
+
+    for mozni_delitelj in range(2, j): #dovolj, je da gremo do vključno korena
+        if j % mozni_delitelj == 0:
+            je_prastevilo = False
+            break # ga ne rabimo ker če je false ne naredimo nič
+
+    if je_prastevilo:
+        print(j) 
