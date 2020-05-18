dcbf3ddd7150e525dbebcc9102ff1ced31746c97
Miha Jan <miha.jan2000@gmail.com>
2020-05-18 20:02:37 +0200

    dodali smo knjižnico bottle, preuredili nekaj datotek.. konec vaj 18.5.2020

---
+++ igra.tpl
@@ -0,0 +1,39 @@
+%import model
+
+
+<!DOCTYPE html>
+<html>
+
+<body>
+
+  <h1>Vislice</h1>
+
+  <blockquote>
+    Vislice so najboljša igra za preganjanje dolgčasa (poleg tetrisa).
+  </blockquote>
+
+  <h2> {{ igra.pravilni_del_gesla() }} </h2>
+
+  <h2> Napačnih ugibov: {{igra.nepravilni_ugibi()}} </h2>
+
+   % if poskus == 'w':
+     <h1> ZMAGAL SI </h1> 
+   % elif poskus == 'x':
+     <h1> IZGUBIL SI </h1>
+   % else: 
+
+
+  <form action="/igra/{{id_igre}}/" method="post">
+   Črka: <input type="text" name="crka">
+   <button type="submit">Ugibaj novo črko</button>
+  </form>
+
+
+  <img src="img/10.jpg" alt="obesanje">
+
+  <form action="/igra/" method="post">
+    <button type="submit">Nova igra</button>
+  </form>
+</body>
+
+</html>
--- model.py
+++ model.py
@@ -3,8 +3,11 @@ import random
 
 # najprej nastavimo konstante
 
-#konstante za rezultate ugibanj
 STEVILO_DOVOLJENIH_NAPAK = 10
+
+ZACETEK = 'Z' 
+
+#konstante za rezultate ugibanj
 PRAVILNA_CRKA = '+'
 PONOVLJENA_CRKA = 'o'
 NAPACNA_CRKA = '-'
@@ -85,7 +88,50 @@ def nova_igra():
     nakljucna_beseda = random.choice(bazen_besed)
     return Igra(nakljucna_beseda)
 
+
+class Vislice:
+    """
+    Skrbi za trenutno stanje več iger (imel bo več objektov tipa igra)
+    """
+    def __init__(self):
+        # Slovar, ki ID-ju priredi objekt njegove igre
+        self.igre = {}    # int -> (Igra, stanje)
+    
+    def prosti_id_igre(self):
+        """ vrne nek ID, ki ga ne uporablja še nobena"""
+        if len(self.igre) == 0:
+            return 0
+        else:
+            return max(self.igre.keys()) + 1
+
+    def nova_igra(self):        
+        # ideja: dobimo svež id
+        nov_id = self.prosti_id_igre()
         
+        # naredimo novo igro
+        sveza_igra = nova_igra()
+
+        # vse to shranimo v self.igre
+        self.igre[nov_id] = (sveza_igra, ZACETEK)
+
+        # vrnemo nov id
+        return nov_id
+
+
+    def ugibaj(self, id_igre, crka):
+        # dobimo staro igro ven
+        trenutna_igra, _ = self.igre[id_igre]
+
+        #ugibamo crko
+        novo_stanje = trenutna_igra.ugibaj(crka)
+
+        # Zapišemo posodobljrno stanje in igro nazaj v "BAZO"  
+        self.igre[id_igre] = (trenutna_igra, nova_igra)
+
+
+     
+            
+
     
 
 
